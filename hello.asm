1aaee742002ae8af6eb1755ba70f573b3f3f629e
chayanne <chayanne.morango02@gmail.com>
2023-11-09 11:44:39 -0300

    Foi gerado o arquivo binário e o executavel

--- hello.asm
+++ hello.asm
@@ -1,9 +1,9 @@
 section .text
-    global_start            ;precisa ser adicionado para o gerador do executavel saiba onde começa
+    global _start            ;precisa ser adicionado para o gerador do executavel saiba onde começa
 
 _start:
     mov edx,len             ;tamanho do texto.len(length-> comprimento)
-    mov ecs,msg             ;mensagem de texto para exibir
+    mov ecx,msg             ;mensagem de texto para exibir
     mov ebx,1               ;descritor de arquivo
     mov eax,4               ;chamada para escrever o texto na tela
     int 0x80                ;chama o terminal para exibir o texto
